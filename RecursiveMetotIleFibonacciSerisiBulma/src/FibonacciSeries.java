c8b93a926fa794abae9de0fee4af7758da808948
Erdi SALGIN <erdisalgin@yandex.com>
2023-09-19 03:02:30 +0300

    Recursive ile Fibonacci Serisi

---
+++ RecursiveMetotIleFibonacciSerisiBulma/src/FibonacciSeries.java
@@ -0,0 +1,28 @@
+public class FibonacciSeries {
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+
+    public static void main(String[] args) {
+        // Fibonacci Dizisi;
+        // her sayının kendisinden bir önceki sayı ile toplanması ile elde edilen
+        // sayılar serisidir.
+
+        // 1 1 2 3 5 8 13 21 ...
+
+        // f(1) = 1
+        // f(2) = 1
+
+        // f(n) = f(n-1) + f(n-2)
+        // f(6) = f(5) + f(4)
+        // f(5) = f(4) + f(3)
+        // f(4) = f(3) + f(2)
+        // f(3) = f(2) + f(1)
+
+        System.out.println(fib(6));
+    }
+}
