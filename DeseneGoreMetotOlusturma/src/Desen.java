cf1bb9a4a134a04c01a740466a230243e3ecc951
Erdi Salgın <110611268+salginerdi@users.noreply.github.com>
2023-09-20 16:28:46 +0300

    Desen.java

--- DeseneGoreMetotOlusturma/src/Desen.java
+++ DeseneGoreMetotOlusturma/src/Desen.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
 
 public class Desen {
-    static int f(int n) {
+    // recursive metot içerisinde koşullarımızı oluşturuyoruz.
+    static int f(int n, int k) {
         System.out.println(n);
+        // eğer değer 0 veya eksiye düşerse bu koşul sağlanıyor.
         if (n <= 0) {
             int newResult = (n + 5);
             System.out.println(newResult);
+            // yeni sonucu çağırıyoruz.
             return newResult;
         }
-
-        int result = f(n - 5);
-        System.out.println(n + 5);
+        int result = f(n - 5, k);
+        // kullanıcıdan aldığımız ve üzerinde işlem yaptığımız değer ilk değerden düşük olursa
+        // if koşulu çalışıyor.
+        if (n < k) {
+            System.out.println(n + 5);
+            return n;
+        }
+        // son olarak sonucu çağırıyoruz.
         return result;
     }
 
@@ -19,6 +27,11 @@ public class Desen {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Lütfen bir sayı giriniz: ");
         int n = scanner.nextInt();
-        n = f(n);
+        // kullanıcıdan bir değer alıyoruz. Fonksiyon içindeki ilk n değeri bu değeri tutuyor
+        // ve işlemlerimizi bunun üzerinden sağlıyoruz.
+        n = f(n, n);
+
     }
+}
+
 }
